2d92decf46f563a337342d7b35ad9c3adee2a4fc
KINGKONG2808 <vuhung2881999@gmail.com>
2019-10-12 01:55:00 +0700

    update

---
+++ JavaWeb_Day2/javaweb-001/src/main/java/com/dev/test/JavaTest.java
@@ -0,0 +1,8 @@
+package com.dev.test;
+import org.apache.commons.math3.util.MathUtils;
+
+public class JavaTest {
+	public static void main(String[] args) {
+		System.out.println("Eval: " + org.apache.commons.math3.util.MathUtils.TWO_PI);
+	}
+}
